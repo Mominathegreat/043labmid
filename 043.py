d1870bc908461b5a14492a840d31247d383333f0
Momina Tariq <momitariq512@gmail.com>
2025-04-11 15:04:30 +0500

    Add files via upload

---
+++ 043.py
@@ -0,0 +1,10 @@
+# This program prints out a greeting
+
+def greet(name):
+    return f"Hello, {name}!"
+
+# Get user input
+user_name = input("Enter your name: ")
+
+# Print the greeting
+print(greet(user_name))
